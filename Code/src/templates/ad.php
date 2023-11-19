15adcd9b9a0ea9ea9adab543552e17a92d1c86de
Balkghar <hugo.germano8@gmail.com>
2023-11-19 17:18:39 +0100

    wip: uwu

--- Code/src/templates/ad.php
+++ Code/src/templates/ad.php
@@ -1,6 +1,6 @@
 <?php require('header.php') ?>
 <h3><?php echo ($ad->getTitle()) ?></h3>
-<h5>Date de création <?php echo ($ad->getCreationDate()) ?></h5>
+<h5>Date de création :<?php echo ($ad->getCreationDate()) ?></h5>
 <h5>Prix : <?php echo ($ad->getPriceInfo()) ?></h5>
 <p><?php echo ($ad->getDescription()) ?></p>
 <?php require('footer.php') ?>
