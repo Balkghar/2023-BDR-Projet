aabf6c3eed03fcd53b1f2187563e40d368a3bddf
Dominik Saul <dominik@saul.ch>
2023-11-29 11:08:16 +0100

    update uml and finish first version of create sql

--- Code/src/sql_scripts/create.sql
+++ Code/src/sql_scripts/create.sql
@@ -1,14 +1,17 @@
+DROP TYPE IF EXISTS Status;
 CREATE TYPE Status AS ENUM (
     'ACTIVE',
     'INACTIVE',
     'DELETED'
     );
 
+DROP TYPE IF EXISTS PaymentMethod;
 CREATE TYPE PaymentMethod AS ENUM (
     'TWINT',
     'CASH'
     );
 
+DROP TYPE IF EXISTS StatusRental;
 CREATE TYPE StatusRental AS ENUM (
     'RESERVATION_ASKED',
     'RESERVATION_CONFIRMED',
@@ -19,6 +22,14 @@ CREATE TYPE StatusRental AS ENUM (
     'LOCATION_FINISHED'
     );
 
+DROP TYPE IF EXISTS PriceInterval;
+CREATE TYPE PriceInterval AS ENUM (
+    'DAY',
+    'WEEK',
+    'MONTH'
+    );
+
+DROP TYPE IF EXISTS Canton;
 CREATE TYPE Canton AS ENUM (
     'AG',
     'AI',
@@ -48,30 +59,34 @@ CREATE TYPE Canton AS ENUM (
     'ZH'
     );
 
-CREATE TABLE IF NOT EXISTS City
+DROP TABLE IF EXISTS City CASCADE;
+CREATE TABLE City
 (
-    id     serial,
-    name   varchar(80),
-    canton varchar(80),
+    zip    int,
+    name   varchar(80) NOT NULL,
+    canton Canton      NOT NULL,
 
-    CONSTRAINT PK_City PRIMARY KEY (id)
+    CONSTRAINT PK_City PRIMARY KEY (zip),
+    CONSTRAINT CK_ZIP CHECK (zip >= 1000 && zip <= 9999)
 );
 
-CREATE TABLE IF NOT EXISTS Address
+DROP TABLE IF EXISTS Address CASCADE;
+CREATE TABLE Address
 (
     id           serial,
-    idZip        int NOT NULL,
+    zipCity      int NOT NULL,
     street       varchar(80),
-    streetNumber smallint,
-    CHECK (streetNumber > 0),
+    streetNumber smallint CHECK (streetNumber > 0),
+
     CONSTRAINT PK_Address PRIMARY KEY (id),
-    CONSTRAINT FK_Address_idZip FOREIGN KEY (idZip)
-        REFERENCES City (id)
+    CONSTRAINT FK_Address_idZip FOREIGN KEY (zipCity)
+        REFERENCES City (zip)
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS Category
+DROP TABLE IF EXISTS Category CASCADE;
+CREATE TABLE Category
 (
     name           varchar(80),
     parentCategory varchar(80),
@@ -83,7 +98,8 @@ CREATE TABLE IF NOT EXISTS Category
         ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS "User"
+DROP TABLE IF EXISTS "User" CASCADE;
+CREATE TABLE "User"
 (
     id               serial,
     idAddress        int,
@@ -95,25 +111,28 @@ CREATE TABLE IF NOT EXISTS "User"
     phoneNumber      varchar(20) NOT NULL,
     status           Status      NOT NULL,
 
-
     CONSTRAINT PK_User PRIMARY KEY (id),
     CONSTRAINT FK_User_Address FOREIGN KEY (idAddress)
         REFERENCES Address (id)
         ON UPDATE RESTRICT
-        ON DELETE RESTRICT
+        ON DELETE RESTRICT,
+    CONSTRAINT CK_mail CHECK (mail like '%@%.%'),
+    CONSTRAINT CK_phoneNumber CHECK (phoneNumber like '+41%[0-9]{9}')
 );
 
-
-CREATE TABLE IF NOT EXISTS Advertisement
+DROP TABLE IF EXISTS Advertisement CASCADE;
+CREATE TABLE Advertisement
 (
-    id           serial,
-    idAddress    integer     NOT NULL,
-    idUser       integer     NOT NULL,
-    nameCategory integer     NOT NULL,
-    creationDate timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title        varchar(80) NOT NULL,
-    description  text        NOT NULL,
-    -- TODO: add description, priceInfo, status
+    id            serial,
+    idAddress     integer       NOT NULL,
+    idUser        integer       NOT NULL,
+    nameCategory  integer       NOT NULL,
+    creationDate  timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title         varchar(80)   NOT NULL,
+    description   text          NOT NULL,
+    price         real          NOT NULL CHECK (price >= 0),
+    priceInterval PriceInterval NOT NULL,
+    status        Status        NOT NULL,
 
     CONSTRAINT PK_Advertisement PRIMARY KEY (id),
     CONSTRAINT FK_Advertisement_idAddress FOREIGN KEY (idAddress)
@@ -127,10 +146,12 @@ CREATE TABLE IF NOT EXISTS Advertisement
     CONSTRAINT FK_Advertisement_nameCategory FOREIGN KEY (nameCategory)
         REFERENCES Category (name)
         ON UPDATE RESTRICT
-        ON DELETE RESTRICT
+        ON DELETE RESTRICT,
+    CONSTRAINT CK_creationDate CHECK (creationDate >= CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE IF NOT EXISTS Rental
+DROP TABLE IF EXISTS Rental CASCADE;
+CREATE TABLE Rental
 (
     id              serial,
     idUser          int           NOT NULL,
@@ -151,10 +172,14 @@ CREATE TABLE IF NOT EXISTS Rental
     CONSTRAINT FK_Rental_Advertisement FOREIGN KEY (idAdvertisement)
         REFERENCES Advertisement (id)
         ON UPDATE RESTRICT
-        ON DELETE RESTRICT
+        ON DELETE RESTRICT,
+    CONSTRAINT CK_creationDate CHECK (creationDate >= CURRENT_TIMESTAMP),
+    CONSTRAINT CK_startDate CHECK (startDate >= CURRENT_TIMESTAMP),
+    CONSTRAINT CK_endDate CHECK (endDate > startDate)
 );
 
-CREATE TABLE IF NOT EXISTS Rating
+DROP TABLE IF EXISTS Rating CASCADE;
+CREATE TABLE Rating
 (
     id           serial,
     idUser       int       NOT NULL,
@@ -162,7 +187,6 @@ CREATE TABLE IF NOT EXISTS Rating
     ratingDate   timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     rentalRating smallint  NOT NULL,
     objectRATING smallint,
-
     CONSTRAINT PK_Rating PRIMARY KEY (id),
     CONSTRAINT FK_Rating_User FOREIGN KEY (idUser)
         REFERENCES "User" (id)
@@ -171,5 +195,7 @@ CREATE TABLE IF NOT EXISTS Rating
     CONSTRAINT FK_Rating_Rental FOREIGN KEY (idRental)
         REFERENCES Rental (id)
         ON UPDATE RESTRICT
-        ON DELETE RESTRICT
+        ON DELETE RESTRICT,
+    CONSTRAINT CK_rentalRating CHECK (rentalRating > 0 && rentalRating < 6),
+    CONSTRAINT CK_objectRating CHECK (objectRating > 0 && objectRating < 6)
 );
