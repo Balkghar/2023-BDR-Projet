407ee3fb659ff17c45af189d88b9e7ff67d39068
Glodi <Glodi_Domingos@hotmail.com>
2023-12-12 16:31:56 +0100

    Deleted comment on constraint already done in constraint.sql

--- Code/src/sql_scripts/constraint.sql
+++ Code/src/sql_scripts/constraint.sql
@@ -1,6 +1,6 @@
--- Un User ne peus pas louer ses propres Advertisement
+-- Un User ne peut pas louer ses propres Advertisement
 
--- On ne peut pas créer un Rental avec le startDate dans le passé (donc après la creationDate). Le startDate d’un Rental doit être avant le endDate. Le paymentDate doit être après la creationDate.
+-- Le startDate d’un Rental doit être avant le endDate. Le paymentDate doit être après la creationDate.
 
 -- Category ne peut pas être une sub-category de lui-même.
 
@@ -16,8 +16,6 @@
 
 -- La date d’un Rating doit être après la creationDate d’un Rental
 
--- Un Rating est un smallint et peut seulement avoir une value de 1-5
-
 -- Le User propriétaire d’un Advertisement peut seulement noter rentalRating dans le Rating
 
 -- Le User locataire d’un Rental doit noter le rentalRating et le objectRating dans le Rating
@@ -25,9 +23,3 @@
 -- Le streetNumber dans Address doit être strictement positif
 
 -- Le price dans Advertisement ne peut pas être négatif
-
--- Le zip dans City ne peut pas être négatif et doit être 4 chiffres
-
--- Le phoneNumber dans User doit être dans un format valide ('+41%[0-9]{9}')
-
--- Le mail dans User doit être dans un format valide ('%@%.%')
