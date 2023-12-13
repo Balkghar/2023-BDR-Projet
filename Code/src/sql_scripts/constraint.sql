ccbb5613420ad79dc57ff1b1941c9ae2c5ea29ee
Glodi <Glodi_Domingos@hotmail.com>
2023-12-13 10:08:30 +0100

    First constraint trigger on Rental & Advert creation (NOT TESTED YET)

--- Code/src/sql_scripts/constraint.sql
+++ Code/src/sql_scripts/constraint.sql
@@ -1,10 +1,23 @@
 -- Un User ne peut pas louer ses propres Advertisement
 
--- Le startDate d’un Rental doit être avant le endDate. Le paymentDate doit être après la creationDate.
-
 -- Category ne peut pas être une sub-category de lui-même.
 
 -- La creationDate de Rental ne peut pas se situer avant la creationDate de Advertisement
+CREATE OR REPLACE FUNCTION chkRentalDate() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS $$
+    DECLARE creationDateAdv timestamp;
+BEGIN
+    SELECT A.creationdate FROM Advertisement AS A WHERE A.id = NEW.idAdvertisement INTO creationDateAdv;
+    IF NEW.cretionDate < creationDateAdv THEN
+        RAISE EXCEPTION 'Rental creation date is before its advertisement creation date';
+    END IF;
+END;
+$$;
+
+CREATE CONSTRAINT TRIGGER checkRentalAdvertCreation AFTER INSERT ON Rental
+    DEFERRABLE INITIALLY DEFERRED
+    FOR EACH ROW EXECUTE FUNCTION chkRentalDate()
 
 -- La creationDate de Rental ne peut pas se situer avant la registrationDate du User
 
