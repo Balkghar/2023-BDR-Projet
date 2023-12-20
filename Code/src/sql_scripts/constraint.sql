d1cab79ba6c741f4767922827de5b452eee242df
Glodi <Glodi_Domingos@hotmail.com>
2023-12-20 11:45:08 +0100

    Date constraint works well now

--- Code/src/sql_scripts/constraint.sql
+++ Code/src/sql_scripts/constraint.sql
@@ -1,37 +1,53 @@
--- Un Profile ne peut pas louer ses propres Advertisement
+-- Un User ne peut pas louer ses propres Advertisement
 
 -- Category ne peut pas être une sub-category de lui-même.
 
 -- La creationDate de Rental ne peut pas se situer avant la creationDate de Advertisement
-CREATE OR REPLACE FUNCTION chkRentalDate() RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION chkDateCongruence() RETURNS TRIGGER
     LANGUAGE plpgsql
 AS $$
-    DECLARE creationDateAdv timestamp;
 BEGIN
-    SELECT A.creationdate FROM Advertisement AS A WHERE A.id = NEW.idAdvertisement INTO creationDateAdv;
-    IF NEW.cretionDate < creationDateAdv THEN
+    IF NEW.creationDate < (SELECT creationDate FROM Advertisement WHERE id = NEW.idAdvertisement) THEN
         RAISE EXCEPTION 'Rental creation date is before its advertisement creation date';
     END IF;
 END;
 $$;
 
-CREATE CONSTRAINT TRIGGER checkRentalAdvertCreation AFTER INSERT ON Rental
+CREATE CONSTRAINT TRIGGER checkRentalAdvertDate AFTER INSERT ON Rental
     DEFERRABLE INITIALLY DEFERRED
-    FOR EACH ROW EXECUTE FUNCTION chkRentalDate()
+    FOR EACH ROW EXECUTE FUNCTION chkDateCongruence();
 
--- La creationDate de Rental ne peut pas se situer avant la registrationDate du Profile
 
--- La registrationDate d’un Profile doit être avant le creationDate d’un Rental d’un Profile
+-- La creationDate de Rental ne peut pas se situer avant la registrationDate du User
+CREATE CONSTRAINT TRIGGER checkRentalUserDate AFTER INSERT ON rental
+    DEFERRABLE INITIALLY DEFERRED
+    FOR EACH ROW EXECUTE FUNCTION chkDateCongruence();
 
--- La registrationDate d’un Profile doit être avant le creationDate d’un Advertisement d’un Profile
+-- La registrationDate d’un User doit être avant le creationDate d’un Rental d’un User
 
--- Un Profile peut seulement faire un Rating pour un Rental pour lequel il est le locataire ou le propriétaire de l’Advertisement.
+-- La registrationDate d’un User doit être avant le creationDate d’un Advertisement d’un User
+
+-- Un User peut seulement faire un Rating pour un Rental pour lequel il est le locataire ou le propriétaire de l’Advertisement.
+--TODO: WIP
+CREATE OR REPLACE FUNCTION chkRating() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF (SELECT idProfile FROM Rental) != NEW.idProfile THEN
+        RAISE EXCEPTION  'Cannot rates Rental where user not part of';
+    end if;
+END
+$$;
+
+CREATE CONSTRAINT TRIGGER checkRating AFTER INSERT ON Rating
+    DEFERRABLE INITIALLY DEFERRED
+    FOR EACH ROW EXECUTE FUNCTION chkRating();
 
 -- La date d’un Rating doit être après la creationDate d’un Rental
 
--- Le Profile propriétaire d’un Advertisement peut seulement noter rentalRating dans le Rating
+-- Le User propriétaire d’un Advertisement peut seulement noter rentalRating dans le Rating
 
--- Le Profile locataire d’un Rental doit noter le rentalRating et le objectRating dans le Rating
+-- Le User locataire d’un Rental doit noter le rentalRating et le objectRating dans le Rating
 
 -- Le streetNumber dans Address doit être strictement positif
 
