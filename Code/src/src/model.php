a341c3403250ffdb0cf58e27640ca14ce29feb28
Balkghar <hugo.germano8@gmail.com>
2024-01-10 09:15:42 +0100

    comment

--- Code/src/src/model.php
+++ Code/src/src/model.php
@@ -405,4 +405,13 @@ class Postgresql
         $condition = ['id' => $idProfile];
         pg_update($this->dbconn, 'profile', $updateQuery, $condition);
     }
+    function getAllCommentsOfProfile($idAd)
+    {
+        $query = "SELECT P.firstname, P.lastname, Ra.comment from rating as Ra 
+                INNER JOIN rental as Re ON Re.id = Ra.idRental 
+                INNER JOIN advertisement as Ad ON Ad.id = Re.idAdvertisement INNER JOIN profile as P on P.id = Ra.idProfile 
+                WHERE Ad.id = $idAd;";
+        $result = $this->query($query);
+        return pg_fetch_all($result);
+    }
 }
